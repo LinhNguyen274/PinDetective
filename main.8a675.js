6b1979e3d7fbbcff41dca675985c3aeddda62bc9
TSernXGamee <sonnht@gameestudio.com>
2024-07-29 10:26:54 +0700

    Update build

--- main.8a675.js
+++ main.8a675.js
@@ -148,7 +148,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.88594.js');
+        require('src/settings.0fc5c.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -156,7 +156,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.88594.js');
+        require('src/settings.0fc5c.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
